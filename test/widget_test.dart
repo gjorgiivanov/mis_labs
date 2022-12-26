34b81ece33f02634dcad1a0d71dfee9c107bc985
gorgiivanov <gorgi.ivanov01@gmail.com>
2022-12-26 14:09:59 +0100

    Folder Structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:mis_labs/main.dart';
+import 'package:mis_labs/lab1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
